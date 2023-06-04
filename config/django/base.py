dd605884c9c8e659b5bb68d567ae3bb059ae80a1
Steven Hong <hongfanmeng@gmail.com>
2023-06-04 08:40:24 +0000

    feat: add filter backend for filtering `not_`

--- config/django/base.py
+++ config/django/base.py
@@ -157,7 +157,7 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 REST_FRAMEWORK = {
     "EXCEPTION_HANDLER": "styleguide_example.api.exception_handlers.drf_default_with_modifications_exception_handler",
     # 'EXCEPTION_HANDLER': 'styleguide_example.api.exception_handlers.hacksoft_proposed_exception_handler',
-    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
+    "DEFAULT_FILTER_BACKENDS": ("styleguide_example.api.filters.FilterBackend",),
     "DEFAULT_AUTHENTICATION_CLASSES": [],
 }
 
